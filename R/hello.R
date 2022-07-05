698f4b9b670238d0f8a89c3fb2c29682ba72281c
JefersonSSouza <47866049+JefersonSSouza@users.noreply.github.com>
2022-07-05 17:59:11 -0300

    test

--- R/hello.R
+++ R/hello.R
@@ -30,7 +30,6 @@ Identify.CellTypes <- function(all.markers.sig,specie='Human',tissue,cancer='Nor
   require('patchwork')
   require('ggforce')
   require('ggrepel')
-  setwd(dirname(rstudioapi::getActiveDocumentContext()$path))# define pasta de trabalho
   celltypes.df <- data.frame(cluster=c(all.markers.sig$cluster),marker=c(all.markers.sig$gene))
   df.marker <- filter(marker_file,Species==specie)
   df.marker <- filter(marker_file,Cell.type==cancer)
