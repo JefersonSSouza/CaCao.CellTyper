99e4226d1f884bca147a003fe93eeb3feb469343
JefersonSSouza <47866049+JefersonSSouza@users.noreply.github.com>
2022-08-01 18:59:38 -0300

    commit

--- R/hello.R
+++ R/hello.R
@@ -71,7 +71,7 @@ seurat.CellTyper <- function(object,CaCao.cluster.percent,rm.celltype=c(), rm.cl
     for(rm.indx in 1:length(rm.celltype)){
     print(paste('removing : ',rm.celltype[rm.indx],sep=''))
     CaCao.cluster.percent <- CaCao.cluster.percent[-which((CaCao.cluster.percent[,'celltype']==rm.celltype[rm.indx])& (CaCao.cluster.percent[,'cluster']==rm.cluster[rm.indx])) ,]
-    print(CaCao.cluster.percent[-which((CaCao.cluster.percent[,'celltype']==rm.celltype[rm.indx])& (CaCao.cluster.percent[,'cluster']==rm.cluster[rm.indx])) ,])
+    
   }
   }
                 
